4505405baea8dd71aba9a4d3e618bde55c852d1a
Matthew Garelli <kzgxmp@usmcdancn2x12x1.onstar.gm.com>
2019-03-18 12:06:32 -0400

    found solution to example directory, still involves copying

--- example/main.py
+++ example/main.py
@@ -1,12 +1,12 @@
 import sys
-from syspy import BashAPI, getInputs, editor
+from .syspy.tools import BashAPI, getInputs, editor
 
-api = BashAPI('exampleApi.sh')
+api = BashAPI('example/api.sh')
 
 # output = api.cmd('copyFile', getInputs())
 # output = api.cmd('falseCommand')
 
-editor('testFolder/file.txt')
+editor('example/testFolder/file.txt')
 
 # print(output)
 
