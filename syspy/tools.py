a893baededeee4a844e4c09840ce891e69e8a526
Matthew Garelli <matthew.garelli@gm.com>
2019-08-28 20:37:08 -0400

    now getting main and resolving symlinks

--- syspy/tools.py
+++ syspy/tools.py
@@ -10,155 +10,156 @@ def succeed(): sys.exit(0)
 def getInputs(): return sys.argv[1:]
 
 def error(msg):
-	context = '\033[91m {}\033[00m'.format('[ERROR] ')
-	print(context + msg)
-	fail()
+  context = '\033[91m {}\033[00m'.format('[ERROR] ')
+  print(context + msg)
+  fail()
 
 def warn(msg):
-	context = '\033[33m {}\033[00m'.format('[WARNING] ')
-	print(context + msg)
+  context = '\033[33m {}\033[00m'.format('[WARNING] ')
+  print(context + msg)
 
 def validate(msg):
-	context = '\033[92m {}\033[00m'.format('[LIT] ')
-	print(context + msg)
+  context = '\033[92m {}\033[00m'.format('[LIT] ')
+  print(context + msg)
 
 def extend(loc, extenstion):
-	return os.path.join(loc, extenstion)
+  return os.path.join(loc, extenstion)
 
 def _which_os():
-	if sys.platform == 'linux' or sys.platform == 'linux2':
-		return 'linux'
-	elif sys.platform == 'darwin':
-		return 'mac'
-	elif sys.platform == 'win32':
-		return 'windows'
-	else:
-		return None
+  if sys.platform == 'linux' or sys.platform == 'linux2':
+    return 'linux'
+  elif sys.platform == 'darwin':
+    return 'mac'
+  elif sys.platform == 'win32':
+    return 'windows'
+  else:
+    return None
 
 class _Find():
-	def __init__(self, shell):
-		self.sh = shell
+  def __init__(self, shell):
+    self.sh = shell
 
-	def directories_with(self, pattern, path=None):
-		# TODO: increase efficiency, possibly using walk
-		all_file_instances = self.recurse(pattern, path=path)
-		all_dirs = [self.sh.dirname(p) for p in all_file_instances]
-		dirs_without_repeats = list(set(all_dirs))
-		return dirs_without_repeats
+  def directories_with(self, pattern, path=None):
+    # TODO: increase efficiency, possibly using walk
+    all_file_instances = self.recurse(pattern, path=path)
+    all_dirs = [self.sh.dirname(p) for p in all_file_instances]
+    dirs_without_repeats = list(set(all_dirs))
+    return dirs_without_repeats
 
-	def here(self, pattern, path=None):
-		if path != None: self.sh.cd(path)
-		if self.sh.verbose:
-			print('Searching one level in directory: ', path if path else self.sh.working)
-			print('\tfor pattern: ', pattern)
-		return glob(pattern, recursive=False)
+  def here(self, pattern, path=None):
+    if path != None: self.sh.cd(path)
+    if self.sh.verbose:
+      print('Searching one level in directory: ', path if path else self.sh.working)
+      print('\tfor pattern: ', pattern)
+    return glob(pattern, recursive=False)
 
-	def recurse(self, pattern, path=None):
-		if path != None: self.sh.cd(path)
-		if self.sh.verbose:
-			print('Searching recursively in directory: ', path if path else self.sh.working)
-			print('\tfor pattern: ', pattern)
-		return glob('**/' + pattern, recursive=True)
+  def recurse(self, pattern, path=None):
+    if path != None: self.sh.cd(path)
+    if self.sh.verbose:
+      print('Searching recursively in directory: ', path if path else self.sh.working)
+      print('\tfor pattern: ', pattern)
+    return glob('**/' + pattern, recursive=True)
 
 
 class Shell():
-	def __init__(self):
-		# relevant directories
-		self.home = os.path.expanduser('~')
-		self.working = os.getcwd()
-		self.main = None
-		try:
-			mainFile = os.path.abspath(sys.modules['__main__'].__file__)
-			self.main = os.path.dirname(mainFile)
-		except: warn('no main file path, interactive interpreter')
-		self.find = _Find(self)
-		# option from command line
-		self.verbose = False
-		# operating system type
-		self.os = _which_os()
+  def __init__(self):
+    # relevant directories
+    self.home = os.path.expanduser('~')
+    self.working = os.getcwd()
+    self.main = None
+    try:
+      exeFile = os.path.abspath(sys.modules['__main__'].__file__)
+      srcFile = os.path.realpath(exeFile) # resolve symlinks
+      self.main = os.path.dirname(srcFile)
+    except: warn('no main file path, interactive interpreter')
+    self.find = _Find(self)
+    # option from command line
+    self.verbose = False
+    # operating system type
+    self.os = _which_os()
 
-	def basename(self, path):
-		basename = os.path.basename(path)
-		if self.verbose: print('Basename of ', path, ' is ', basename)
-		return basename
+  def basename(self, path):
+    basename = os.path.basename(path)
+    if self.verbose: print('Basename of ', path, ' is ', basename)
+    return basename
 
-	def cd(self, path):
-		if self.verbose: print('Changing directory to: ', path)
-		os.chdir(path)
+  def cd(self, path):
+    if self.verbose: print('Changing directory to: ', path)
+    os.chdir(path)
 
-	def chrome(self, url):
-		if self.os == 'linux':
-			self.command([
-				'/usr/bin/chromium-browser',
-				'--disable-features=NetworkService',
-				url,
-				'&>/dev/null'
-				])
-		elif self.os == 'mac':
-			error('no mac implementation yet')
+  def chrome(self, url):
+    if self.os == 'linux':
+      self.command([
+        '/usr/bin/chromium-browser',
+        '--disable-features=NetworkService',
+        url,
+        '&>/dev/null'
+        ])
+    elif self.os == 'mac':
+      error('no mac implementation yet')
 
-	def command(self, cmd_list):
-		cmd = ' '.join(cmd_list)
-		if self.verbose: print(cmd)
-		os.system(cmd)
+  def command(self, cmd_list):
+    cmd = ' '.join(cmd_list)
+    if self.verbose: print(cmd)
+    os.system(cmd)
 
-	def dirname(self, path):
-		dirname = os.path.dirname(path)
-		if self.verbose: print('Dirname of ', path, ' is ', dirname)
-		return dirname
+  def dirname(self, path):
+    dirname = os.path.dirname(path)
+    if self.verbose: print('Dirname of ', path, ' is ', dirname)
+    return dirname
 
-	def exists(self, path): # check for path of directory
-		if self.verbose: print('Checking the existance of path: ', path)
-		return os.path.exists(path)
+  def exists(self, path): # check for path of directory
+    if self.verbose: print('Checking the existance of path: ', path)
+    return os.path.exists(path)
 
-	def link(self, src, dest):
-		os.symlink(src, dest)
+  def link(self, src, dest):
+    os.symlink(src, dest)
 
-	def ls(self, path):
-		if self.verbose: print('Listing files in directory: ', path)
-		return os.listdir(path)
+  def ls(self, path):
+    if self.verbose: print('Listing files in directory: ', path)
+    return os.listdir(path)
 
-	def make_executable(self, file):
-		self.command(['chmod +x', file])
+  def make_executable(self, file):
+    self.command(['chmod +x', file])
 
-	def mkdir(self, path):
-		try:
-			os.mkdir(path)
-			if self.verbose: print('made directory: ', path)
-		except:
-			if self.verbose: print('failed to make directory: ', path)
+  def mkdir(self, path):
+    try:
+      os.mkdir(path)
+      if self.verbose: print('made directory: ', path)
+    except:
+      if self.verbose: print('failed to make directory: ', path)
 
-	def rm(self, file):
-		if self.verbose: print('removing: ', file)
-		os.remove(file)
+  def rm(self, file):
+    if self.verbose: print('removing: ', file)
+    os.remove(file)
 
 def parseOptions(args, shortOpts, longOpts):
-	try:
-		options, remainder = getopt.getopt(
-			args,
-			shortOpts,
-			longOpts
-			)
-		command = None
-		try:
-			command = remainder[0]
-			remainder = remainder[1:]
-		except:
-			remainder = None
-		return options, command, remainder
-	except getopt.GetoptError as err:
-		print(err)
-		error('parsing the options failed')
+  try:
+    options, remainder = getopt.getopt(
+      args,
+      shortOpts,
+      longOpts
+      )
+    command = None
+    try:
+      command = remainder[0]
+      remainder = remainder[1:]
+    except:
+      remainder = None
+    return options, command, remainder
+  except getopt.GetoptError as err:
+    print(err)
+    error('parsing the options failed')
 
 def vim(name):
-	vim = os.environ.get('EDITOR', 'vim') # create editor
-	def open_file_with_vim(permission):
-		with open(name, permission) as tf:
-			call([vim, tf.name])
-	try:
-		open_file_with_vim('r+') # open file to read
-	except:
-		open_file_with_vim('w+') # open file to write
+  vim = os.environ.get('EDITOR', 'vim') # create editor
+  def open_file_with_vim(permission):
+    with open(name, permission) as tf:
+      call([vim, tf.name])
+  try:
+    open_file_with_vim('r+') # open file to read
+  except:
+    open_file_with_vim('w+') # open file to write
 
 # supressing stdout
 # https://stackoverflow.com/questions/2828953/silence-the-stdout-of-a-function-in-python-without-trashing-sys-stdout-and-resto
