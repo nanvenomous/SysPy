bb6a7c181fae9df9937c90837ef9a57883997b50
Matthew Garelli <mgarelli@alumni.stanford.edu>
2019-09-02 13:55:36 -0400

    added support for chrome on mac os

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
 	name="syspy",
-	version="0.0.44",
+	version="0.0.45",
 	author="Matthew Garelli",
 	author_email="mgarelli@alumni.stanford.edu",
 	description="A module to do certain unix system tasks",
--- syspy/runExample.py
+++ syspy/runExample.py
@@ -3,67 +3,68 @@ from tools import getInputs, vim, Shell, validate, extend
 sh = Shell()
 
 def get_home_directory():
-  print('#### get home directory')
-  sh.verbose = True
-  print(sh.home)
-  new_path = extend(sh.home, 'fromHome')
-  print(new_path)
-  sh.dirname(new_path)
-  sh.basename(new_path)
-  sh.verbose = False
-  print()
+	print('#### get home directory')
+	sh.verbose = True
+	print(sh.home)
+	new_path = extend(sh.home, 'fromHome')
+	print(new_path)
+	sh.dirname(new_path)
+	sh.basename(new_path)
+	sh.verbose = False
+	print()
 
 def remove_file():
-  print('#### Remove file')
-  sh.rm('example/testFolder/file.txt')
-  print()
+	print('#### Remove file')
+	sh.rm('example/testFolder/file.txt')
+	print()
 
 def create_directory_if_nonexistent():
-  print('#### create directory if it doesnt exist')
-  sh.mkdir(extend(sh.main, 'example/testFolder'))
-  print()
+	print('#### create directory if it doesnt exist')
+	sh.mkdir(extend(sh.main, 'example/testFolder'))
+	print()
 
 def open_file_in_editor():
-  print('#### Check Editor Operation')
-  vim('example/testFolder/file.txt')
-  print()
+	print('#### Check Editor Operation')
+	vim('example/testFolder/file.txt')
+	print()
 
 def print_example_validation():
-  print('#### Print a validation message')
-  validate('things are looking good for you')
-  print()
+	print('#### Print a validation message')
+	validate('things are looking good for you')
+	print()
 
 def make_symbolic_link():
-  print('#### Create a symlink')
-  sh.link('example/testFolder/file.txt', 'ex')
-  print()
+	print('#### Create a symlink')
+	sh.link('example/testFolder/file.txt', 'ex')
+	print()
 
 def make_file_executable():
-  print('#### Make file executable')
-  sh.make_executable('example/testFolder/file.txt')
-  print()
+	print('#### Make file executable')
+	sh.make_executable('example/testFolder/file.txt')
+	print()
 
 def list_files_in_directory():
-  print('#### List all files in a directory')
-  dir_files = sh.ls(sh.main)
-  print(dir_files)
-  print()
+	print('#### List all files in a directory')
+	dir_files = sh.ls(sh.main)
+	print(dir_files)
+	print()
 
 def open_file_in_browser():
-  print('#### Open file in browser')
-  # sh.chrome('example/ex.md')
-  sh.chrome('dogs.com')
-  print()
+	print('#### Open file in browser')
+	# sh.chrome('example/ex.md')
+	sh.verbose = True
+	sh.chrome('./example/ex.md')
+	print()
 
 def find_pattern_recursively_in_directory():
-  print('#### find_pattern_recursively_in_directory')
-  print(sh.find.recurse('*.py'))
-  print()
+	print('#### find_pattern_recursively_in_directory')
+	print(sh.find.recurse('*.py'))
+	print()
 
 def find_directories_containing_file_with_pattern():
-  print('#### find_directories_containing_file_with_pattern')
-  print(sh.find.directories_with('*.txt'))
-  print()
+	print('#### find_directories_containing_file_with_pattern')
+	print(sh.find.directories_with('*.txt'))
+	print()
 
 get_home_directory()
 # remove_file()
@@ -73,6 +74,6 @@ print_example_validation()
 # make_symbolic_link()
 # make_file_executable()
 list_files_in_directory()
-# open_file_in_browser()
 find_pattern_recursively_in_directory()
 find_directories_containing_file_with_pattern()
+open_file_in_browser()
